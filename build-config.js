104eabf98449c7c6ce03a58376c8575cfccef43b
Joshua Flark <joshuajmark@gmail.com>
2018-06-08 22:56:29 -0700

    All headers supported

--- build-config.js
+++ build-config.js
@@ -79,7 +79,27 @@ module.exports = {
 				'alignLeft',
 				'alignRight'
 			],
-			toolbar: [ 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight', '|', 'imageTextAlternative', '|', 'comment' ]
+			toolbar: [
+				'imageStyle:alignLeft',
+				'imageStyle:full',
+				'imageStyle:alignRight',
+				'|',
+				'imageTextAlternative',
+				'|',
+				'comment'
+			]
+		},
+
+		heading: {
+		  options: [
+		    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+		    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+		    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+		    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+		    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+		    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+		    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+		  ]
 		},
 
 		// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
